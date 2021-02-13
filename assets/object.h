282c9ba14cd1532df209df1603a9593614a6124b
AlfredEVOL <57448041+AlfredEVOL@users.noreply.github.com>
2021-02-13 22:59:04 +0530

    .

---
+++ assets/object.h
@@ -0,0 +1,87 @@
+#include "objectsGraphics.h"
+
+class object
+{
+private:
+    float x, y, prev_x, prev_y;
+    float velocity_x, velocity_y;
+    int height, width;
+    float strength; //damage/health
+
+    ObjectGraphics *Obj;
+
+public:
+    object *f_link, *b_link;
+    object(/* args */);
+    ~object();
+
+    void drawObject(WINDOW *win)
+    {
+        Obj->eraseObject(prev_y, prev_x, win);
+        Obj->drawObject(y, x, prev);
+    }
+    void getDimensions(char &h, char &w)
+    {
+        Obj->getDimensions(h, w);
+    }
+    float getStrength()
+    {
+        return strength;
+    }
+    float getx()
+    {
+        return x;
+    }
+    float gety()
+    {
+        return y;
+    }
+    float getPrev_x()
+    {
+        return prev_x;
+    }
+    float getPrev_y()
+    {
+        return prev_y;
+    }
+    void move()
+    {
+        //add velocity*dt to both coords
+        //check boundary conditions
+    }
+    void revVelocity()
+    {
+        velocity_x *= -1;
+    }
+};
+
+object::object(float x, float y, int type)
+{
+    //initialize object graphic
+    //get dimensions
+    //assign rest of attributes
+}
+
+object::~object()
+{
+    //clear memory
+    f_link = b_link = nullptr;
+    Obj = nullptr;
+}
+
+class ObjectList
+{
+private:
+    /* data */
+public:
+    ObjectList(/* args */);
+    ~ObjectList();
+};
+
+ObjectList::ObjectList(/* args */)
+{
+}
+
+ObjectList::~ObjectList()
+{
+}
--- learningncurses/test2.cpp
+++ learningncurses/test2.cpp
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 
-#include "objects.h"
+#include "objectsGraphics.h"
 #include "colors.h"
 #include "screen.h"
 #define delay(ms) usleep(ms*1000)
@@ -14,37 +14,37 @@ int main()
     ObjectGraphics SHIP(0);
     int ch = 'y';
     int x = 10, y = 10;
-    // SHIP.drawObject(y, x);
-    // refresh();
-    // while ((ch = getch()) && ch != 'q')
-    // {
-    //     SHIP.eraseObject(y, x);
-    //     switch (ch)
-    //     {
-    //     case KEY_LEFT:
-    //         --x;
-    //         break;
-    //     case KEY_UP:
-    //         --y;
-    //         break;
-    //     case KEY_DOWN:
-    //         ++y;
-    //         break;
-    //     case KEY_RIGHT:
-    //         ++x;
-    //         break;
-    //     }
-    //     SHIP.drawObject(y, x);
-    // }
-
-    
-    for (int i = 0; i < COLS; ++i)
+    SHIP.drawObject(y, x);
+    refresh();
+    while ((ch = getch()) && ch != 'q')
     {
-        SHIP.drawObject(LINES / 2, i);
-        refresh();
-        delay(40);
-        SHIP.eraseObject(LINES / 2, i);
+        SHIP.eraseObject(y, x);
+        switch (ch)
+        {
+        case KEY_LEFT:
+            --x;
+            break;
+        case KEY_UP:
+            --y;
+            break;
+        case KEY_DOWN:
+            ++y;
+            break;
+        case KEY_RIGHT:
+            ++x;
+            break;
+        }
+        SHIP.drawObject(y, x);
     }
+
+
+    // for (int i = 0; i < COLS; ++i)
+    // {
+    //     SHIP.drawObject(LINES / 2, i);
+    //     refresh();
+    //     delay(40);
+    //     SHIP.eraseObject(LINES / 2, i);
+    // }
     endwin();
     return 0;
 }
